d376147e1ac2a73a09cb02fc7c53d0d1dca4d46f
AlitaChu <scriptales.clients@gmail.com>
2020-02-04 14:59:22 +0100

    Gestion de toutes les questions

--- script.js
+++ script.js
@@ -12,30 +12,48 @@ titre[0].addEventListener("mouseout", function (e) {
     titre[0].style.color = "green";
 });*/
 
+let questions = document.getElementsByClassName("question");
+let max = questions.length;
+let points = 0;
+let count = max;
 
-var question1 = document.getElementById('question-1');
-question1.lastElementChild.addEventListener("submit", function (event) {
-    // Pour stopper le comportement par défaut, et empêcher la page de se recharger
-    event.preventDefault();
-    
-    var elems = document.getElementsByName('question-1');
-    var max = elems.length;
+for(let i = 0; i < max; i++) {
 
-    // Pour chaque élément "input type radio" :
-    for(let i = 0; i < max; i++) {
-        // Lorsque l'utilisateur a validé, les réponses ne peuvent plus être modifiées
-        // Les boutons radio sont donc désactivés
-        elems[i].disabled = true;
+    questions[i].lastElementChild.addEventListener("submit", function (event) {
+        // Pour stopper le comportement par défaut, et empêcher la page de se recharger
+        event.preventDefault();
+        
+        var elems = document.getElementsByName('question-' + (i + 1));
+        var max = elems.length;
+        count--;
 
-        if(elems[i].checked && elems[i].value == 'true') {
-            elems[i].parentElement.parentElement.style.color = "green";
-            elems[i].previousElementSibling.style.fontWeight = "bold";
-            elems[i].previousElementSibling.style.fontSize = "1.1em";
+        // Pour chaque élément "input type radio" :
+        for(let i = 0; i < max; i++) {
+            // Lorsque l'utilisateur a validé, les réponses ne peuvent plus être modifiées
+            // Les boutons radio sont donc désactivés
+            elems[i].disabled = true;
 
-        } else if (elems[i].checked && elems[i].value == 'false') {
-            elems[i].parentElement.parentElement.style.color = "red";
-            elems[i].previousElementSibling.style.fontWeight = "bold";
-            elems[i].previousElementSibling.style.fontSize = "1.1em";
+            if(elems[i].checked && elems[i].value == 'true') {
+                elems[i].parentElement.parentElement.style.color = "green";
+                elems[i].previousElementSibling.style.fontWeight = "bold";
+                elems[i].previousElementSibling.style.fontSize = "1.1em";
+                points++;
+            } else if (elems[i].checked && elems[i].value == 'false') {
+                elems[i].parentElement.parentElement.style.color = "red";
+                elems[i].previousElementSibling.style.fontWeight = "bold";
+                elems[i].previousElementSibling.style.fontSize = "1.1em";
+            }
         }
+        checkEnd();
+        
+    });
+}
+
+function checkEnd() {
+
+    if(count === 0) {
+        var fin = document.getElementById("fin");
+        fin.innerHTML = "<p>Vous avez répondu à toutes les questions<br>Votre score est de " + points + "/" + max + "</p>"
+        fin.style.display = "block";
     }
-});
+}
