4980a81461509bf30f40224130533078a42fba39
AlitaChu <scriptales.clients@gmail.com>
2020-02-04 14:39:36 +0100

    Condition de "victoire" implémentée

--- script.js
+++ script.js
@@ -13,17 +13,29 @@ titre[0].addEventListener("mouseout", function (e) {
 });*/
 
 
-var question1 = document.getElementById("question-1");
-console.log(question1.lastElementChild);
+var question1 = document.getElementById('question-1');
 question1.lastElementChild.addEventListener("submit", function (event) {
     // Pour stopper le comportement par défaut, et empêcher la page de se recharger
     event.preventDefault();
     
     var elems = document.getElementsByName('question-1');
     var max = elems.length;
+
+    // Pour chaque élément "input type radio" :
     for(let i = 0; i < max; i++) {
-        console.log("La réponse " + elems[i].id + " a la valeur " + elems[i].value + " et est cochée : " + elems[i].checked);
+        // Lorsque l'utilisateur a validé, les réponses ne peuvent plus être modifiées
+        // Les boutons radio sont donc désactivés
+        elems[i].disabled = true;
+
+        if(elems[i].checked && elems[i].value == 'true') {
+            elems[i].parentElement.parentElement.style.color = "green";
+            elems[i].previousElementSibling.style.fontWeight = "bold";
+            elems[i].previousElementSibling.style.fontSize = "1.1em";
+
+        } else if (elems[i].checked && elems[i].value == 'false') {
+            elems[i].parentElement.parentElement.style.color = "red";
+            elems[i].previousElementSibling.style.fontWeight = "bold";
+            elems[i].previousElementSibling.style.fontSize = "1.1em";
+        }
     }
-
 });
-
