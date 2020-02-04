c43d458e00273a757fd5439d0ca9bbb5ebcb6992
AlitaChu <scriptales.clients@gmail.com>
2020-02-04 16:24:48 +0100

    Ajout bouton de fermeture de la modale + structuration des questions en JS

--- script.js
+++ script.js
@@ -53,7 +53,59 @@ function checkEnd() {
 
     if(count === 0) {
         var fin = document.getElementById("fin");
-        fin.innerHTML = "<p>Vous avez répondu à toutes les questions<br>Votre score est de " + points + "/" + max + "</p>"
+        var info = document.getElementById("info");
+        info.innerHTML = "<span id=\"close\">&#x2716;</span>Vous avez répondu à toutes les questions<br>Votre score est de " + points + "/" + max ;
         fin.style.display = "block";
+        document.getElementById("close").addEventListener('click', function (e) {
+            e.target.parentElement.parentElement.style.display = "none";
+        });
     }
 }
+
+// Questions structurées :
+
+question1 = {
+    id: 1,
+    intitule: "Un acouphène est une sensation qui affecte quel organe ?",
+    reponses: [
+        { id: 1, label: "Oeil", value: false },
+        { id: 2, label: "Bouche", value: false },
+        { id: 3, label: "Oreille", value: true },
+        { id: 4, label: "Nez", value: false }
+    ]
+}
+
+question2 = {
+    id: 2,
+    intitule: "La mère de Lucie a trois enfants, ces deux premières filles se nomment Marie et Anna. Comment s'appelle son troisième enfant ?",
+    reponses: [
+        { id: 1, label: "Lucie", value: true },
+        { id: 2, label: "Delphine", value: false },
+        { id: 3, label: "Jaqueline", value: false },
+        { id: 4, label: "On ne l'appelle pas on la siffle", value: false }
+    ]
+}
+
+question3 = {
+    id: 3,
+    intitule: "Combien y a t'il de lettres dans l'alphabet cyrillique ?",
+    reponses: [
+        { id: 1, label: 30, value: true },
+        { id: 2, label: 300, value: false },
+        { id: 3, label: 60, value: false },
+        { id: 4, label: 2, value: false }
+    ]
+}
+
+question4 = {
+    id: 4,
+    intitule: "Quelle est la monnaie du Brésil ?",
+    reponses: [
+        { id: 1, label: "Le réal", value: false },
+        { id: 2, label: "L'escudo", value: true },
+        { id: 3, label: "Le dollar brésilien", value: false },
+        { id: 4, label: "Le franc Suisse", value: false }
+    ]
+}
+
+let lst_questions = [question1, question2, question3, question4];
