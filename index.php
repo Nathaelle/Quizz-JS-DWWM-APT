9d372618563e037a830f7830681d3f7ae6996f0d
Nathaelle <scriptales.clients@gmail.com>
2020-09-04 16:27:33 +0200

    Implémentation du back-end 1 : Controller php + template rendering

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+$route = isset($_REQUEST['route'])? $_REQUEST['route'] : "home";
+
+// Routage
+
+switch($route) {
+    case "home" : $view = showHome();
+    break;
+    default : $view = showHome();
+}
+
+// Fonctions de controle
+
+function showHome() {
+
+    return ["template" => "views/home.php"];
+}
+
+
+
+// Template maître
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Quizz </title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+    <?php require($view['template']) ?>
+
+    <script src="questions.js"></script>
+</body>
+</html>
---
+++ views/home.php
@@ -0,0 +1,9 @@
+<h1>Quizz culture générale</h1>
+
+<div id="liste">
+</div>
+
+<div id="fin">
+    <p id="info">
+    </p>
+</div>
